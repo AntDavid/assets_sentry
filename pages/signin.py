f44ebcaaa72d29e40ecfcd7a1214f2d0b1e5aedd
AntDavid <davidantoniodavi@gmail.com>
2024-07-19 12:41:34 -0300

    Use check_password to handle user authentication

--- pages/signin.py
+++ pages/signin.py
@@ -4,18 +4,11 @@ from auth import check_password
 def login():
     st.title("Sign In")
 
-    with st.form(key = 'login_form'):
-        username = st.text_input("Username")
-        password = st.text_input("Password", type = 'password')
-        submit_button = st.form_submit_button(label = 'Sign In')
+    st.text_input("Username", key="username")
+    st.text_input("Password", type="password", key="password")
 
-        if submit_button:
-            st.session_state["Username"] = username
-            st.session_state["Password"] = password
-
-            if check_password(username, password):
-                st.sucess("You are authenticated")
-            
-            else:
-                st.error("Invalid username or password")
-                st.stop()
+    if st.button("Sign In"):
+        if check_password():
+            st.success("You are authenticated")
+        else:
+            st.warning("Invalid username or password")
