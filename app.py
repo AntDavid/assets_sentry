75dd2634c9b18db65db56e72d9524cb7b1ccfd04
AntDavid <davidantoniodavi@gmail.com>
2024-07-19 12:47:58 -0300

    Update main app logic to include navigation to login and signup pages and Update to use session state for authentication

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 import streamlit as st
-from auth import check_password
-from utils.db import create_connection, creat_table
 
+st.write("Welcome to Stock Analysis App")
 
-conn = create_connection("database.db")
-creat_table(conn)
+if "authenticated" not in st.session_state:
+    st.session_state["authenticated"] = False
 
-if check_password():
+if not st.session_state["authenticated"]:
+    st.sidebar.write("Please, select a page to continue")
+    page = st.sidebar.selectbox("Select a page", ["Sign In", "Sign Up"])
+
+    if page == "Sign In":
+        from pages.signin import login
+        login()
+    elif page == "Sign Up":
+        from pages.signup import signup
+        signup()
+else:
     st.write("You are authenticated")
     st.text("You can now access the stock analysis page")
     page = st.sidebar.selectbox("Select a page", ["Stock Analysis"])
     if page == "Stock Analysis":
         from pages.stock_analysis import stock_analysis
         stock_analysis()
-
-    else:
-        st.warning("Invalid username or password. Try again")
-        st.stop()
