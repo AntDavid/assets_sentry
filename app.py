4a98592f4944efacccbf6877047192c4855be7d4
AntDavid <davidantoniodavi@gmail.com>
2024-07-28 04:46:15 -0300

    Add initial page state management and navigation logic using session_state

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 import streamlit as st
 
-st.write("Welcome to Stock Analysis App")
+if "page" not in st.session_state:
+    st.session_state["page"] = "Sign In"
+
+def change_page(page):
+    st.session_state["page"] = page
 
 if "authenticated" not in st.session_state:
     st.session_state["authenticated"] = False
 
-if not st.session_state["authenticated"]:
-    st.sidebar.write("Please, select a page to continue")
-    page = st.sidebar.selectbox("Select a page", ["Sign In", "Sign Up"])
+if st.session_state.page == "Sign In":
+    from views.signin import login
+    login(change_page)
 
-    if page == "Sign In":
-        from pages.signin import login
-        login()
-    elif page == "Sign Up":
-        from pages.signup import signup
-        signup()
-else:
+elif st.session_state.page == "Sign Up":
+    from views.signup import signup
+    signup(change_page)    
+
+elif st.session_state.page == "authenticated":
     st.write("You are authenticated")
-    st.text("You can now access the stock analysis page")
-    page = st.sidebar.selectbox("Select a page", ["Stock Analysis"])
-    if page == "Stock Analysis":
-        from pages.stock_analysis import stock_analysis
-        stock_analysis()
+    from views.stock_analysis import stock_analysis
+    stock_analysis()
+
+
