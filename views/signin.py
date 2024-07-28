539eb38e4d0b0869f192690f8815b5f726f94212
AntDavid <davidantoniodavi@gmail.com>
2024-07-28 05:00:46 -0300

    Implement Sign In form with session_state-based navigation and status messages

--- views/signin.py
+++ views/signin.py
@@ -2,7 +2,7 @@ import streamlit as st
 from auth import check_password
 from streamlit_extras.switch_page_button import switch_page
 
-def login():
+def login(change_page):
     st.title("Sign In")
 
     st.text_input("Username", key="username")
@@ -11,12 +11,17 @@ def login():
     if st.button("Sign In"):
         if check_password():
             st.success("You are authenticated")
-            switch_page("stock analysis")
+            st.session_state["authenticated"] = True
+            change_page("authenticated")
+            st.rerun()
 
-            
         else:
             st.warning("Invalid username or password")
-            st.rerun()
-            
+            st.experimental_rerun()
+  
+  
     if st.button("Sign Up"):
-        switch_page("Sign Up")
+        change_page("Sign Up")
+        st.rerun()
+            
+
