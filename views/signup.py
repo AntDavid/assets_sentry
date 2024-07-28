2701a371b1d97602cbedafd306aebddc53179c87
AntDavid <davidantoniodavi@gmail.com>
2024-07-28 05:01:25 -0300

    Implement Sign Up form with session_state-based navigation and status messages

--- views/signup.py
+++ views/signup.py
@@ -3,31 +3,41 @@ from utils.db import create_connection, add_user
 from streamlit_extras.switch_page_button import switch_page
 
 
-def signup():
+def signup(change_page):
     st.title("Sign Up")
+    page = None
 
-    with st.form(key="signup_form"):
-        name = st.text_input("Name")
-        username = st.text_input("Username")
-        email = st.text_input("Email")
-        password = st.text_input("Password", type="password")
-        confirm_password = st.text_input("Confirm Password", type="password")
-        submit_button = st.form_submit_button(label="Sign Up")
-        login_button = st.form_submit_button(label="Sign In")
 
-        if submit_button:
-            if name and username and email and password and confirm_password:
-                if password == confirm_password:
-                    conn = create_connection("database.db")
-                    user_id = add_user(conn, name, username, email, password)
-                    st.success("Account created successfully")
-                    switch_page("signin")
+    st.form(key="signup_form")
+    name = st.text_input("Name")
+    username = st.text_input("Username")
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
+    confirm_password = st.text_input("Confirm Password", type="password")
+    
+    
 
-                else:
-                    st.error("Passwords do not match")
-
+    if st.button(label="Sign Up"):
+        if name and username and email and password and confirm_password:
+            if password == confirm_password:
+                conn = create_connection("database.db")
+                user_id = add_user(conn, name, username, email, password)
+                st.success("Account created successfully")
+                change_page("Sign In")
+                st.experimental_rerun()
+                
             else:
-                st.error("Please fill in all fields")
+                st.error("Passwords do not match")
 
-        if login_button:
-            switch_page("signin")
+        else:
+            st.error("Please fill in all fields")
+
+
+    if st.button("Sign In"):
+        change_page("Sign In")
+        st.rerun()
+
+
+
+
+       
